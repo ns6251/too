9176267b7934386000732e8b8aef869c7d11822b
Shota Nakagawa <g21210368a@edu.teu.ac.jp>
2022-01-06 16:21:51 +0900

    Add cli opiton (but not implemented)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
-use clap::Parser;
-use futures::future::try_join_all;
+use std::borrow::Cow;
 use std::path::PathBuf;
+
+use clap::{ArgEnum, Parser};
+use futures::future::try_join_all;
+use regex::Regex;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
-struct Cli {
-    file: Vec<PathBuf>,
+pub struct Cli {
+    /// Append to the given FILEs, do not overwrite
+    #[clap(short, long)]
+    pub append: bool,
+
+    /// Ignore interrupt signals
+    #[clap(short, long)]
+    pub ignore_interrupts: bool,
+
+    /// Diagnose errors writing to non pipes
+    #[clap(short)]
+    pub p: bool,
+
+    /// Set behavior on write error
+    #[clap(long, arg_enum, default_value_t = Mode::WarnNopipe)]
+    pub output_error: Mode,
+
+    pub file: Vec<PathBuf>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum Mode {
+    /// Diagnose errors writing to any output
+    Warn,
+
+    /// Diagnose errors writing to any output not a pipe
+    WarnNopipe,
+
+    /// Exit on error writing to any output
+    Exit,
+
+    /// Exit on error writing to any output not a pipe
+    ExitNopipe,
+}
+
+/// Remove ASCII
+fn decolorize(s: &str) -> Cow<str> {
+    let re = Regex::new(r#"\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]"#).unwrap();
+    re.replace_all(s, "")
 }
 
 #[tokio::main]
@@ -20,7 +60,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdout = tokio::io::stdout();
     let stdout = stdout.write(buf.as_bytes());
 
-    let files = cli.file.iter().map(|path| tokio::fs::write(path, &buf));
+    let plain = decolorize(&buf);
+
+    let files = cli
+        .file
+        .iter()
+        .map(|file| tokio::fs::write(file, plain.as_ref()));
     let files = try_join_all(files);
 
     tokio::try_join!(stdout, files)?;
